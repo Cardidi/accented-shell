576118f3dcc732c88c4df6c0ca7f37aaaeebeca5
cardidi <cardidi@live.cn>
2025-02-11 14:59:09 +0800

    Do a simple modification

--- extension.js
+++ extension.js
@@ -1,17 +1,20 @@
 //    Accented panel
 //    GNOME Shell extension
+//    @Cardidi 2025
 //    @fthx 2025
 
-
+import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 
-export default class AccentedPanelExtension {
+export default class AccentedPanelExtension extends Extension{
     enable() {
         Main.panel.add_style_class_name('panel-accent');
+        Main.overview._overview.add_style_class_name('panel-accent');
     }
 
     disable() {
         Main.panel.remove_style_class_name('panel-accent');
+        Main.overview._overview.remove_style_class_name('panel-accent');
     }
 }
